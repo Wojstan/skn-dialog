72bd87b25558d1f31be9b7406c8889256090a865
Krzysztof Wilk <k.wilk@mdbootstrap.com>
2022-05-08 19:43:54 +0200

    Start preparing single post page

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -4,14 +4,41 @@ import parse from "html-react-parser";
 import getPosts from "../../utils/getPosts";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
 
 const Post: NextPage = ({ post }: any) => {
   return (
     <Layout>
-      <Container>
-        <Row>Title: {post.title}</Row>
-        <Row>Content: {parse(post.content)}</Row>
-        <Row>ID: {post._id}</Row>
+      <Container className="p-5">
+        <Row>
+          <h1 className="text-start mt-4">{post.title}</h1>
+        </Row>
+        <Row className="mt-4 mb-5 d-inline-block">
+          <small>
+            Opublikowano: wtorek, {post.publication_date},{" "}
+            {post.publication_time}
+            <span className="ms-3">&#9679;</span>
+          </small>
+          <span>
+            <FontAwesomeIcon
+              className="share-icon"
+              size="lg"
+              icon={faFacebook}
+            />
+            <FontAwesomeIcon
+              className="share-icon mx-3"
+              size="lg"
+              icon={faTwitter}
+            />
+            <FontAwesomeIcon className="share-icon" size="lg" icon={faLink} />
+          </span>
+        </Row>
+        <Row className="mb-5">
+          <img src={post.thumbnail} alt="post-image" className="" />
+        </Row>
+        <Row>{parse(post.content)}</Row>
       </Container>
     </Layout>
   );
