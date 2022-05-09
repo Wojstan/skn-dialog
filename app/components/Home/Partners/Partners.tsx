51618a4426bb6591041afaa763413da385d58e2b
Wojciech Staniszewski <w.staniszewski@mdbootstrap.com>
2022-05-09 18:42:46 +0200

    Add home page

--- app/components/Home/Partners/Partners.tsx
+++ app/components/Home/Partners/Partners.tsx
@@ -1,9 +1,14 @@
 const partners = ["gwp.png", "cpp.png", "kampus.png", "pkn.png", "wuj.png"];
 
 const Partners = () => (
-  <aside className="d-flex justify-content-around align-items-center my-5">
+  <aside className="d-flex flex-column justify-content-around align-items-center my-lg-5 flex-lg-row">
     {partners.map((partner, i) => (
-      <img key={i} src={`/img/partners/${partner}`} alt="" />
+      <img
+        className="mb-lg-0 mb-5"
+        key={i}
+        src={`/img/partners/${partner}`}
+        alt=""
+      />
     ))}
   </aside>
 );
