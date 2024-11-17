af5480ea5b48425ee39fbe3f165b2e0af2d5246e
vivalavicc <wvkopecka@gmail.com>
2024-11-17 13:21:31 +0100

    Add 24/25 changes

--- src/constants/About.ts
+++ src/constants/About.ts
@@ -5,32 +5,32 @@ export const header = {
 
 export const protector = {
   title: 'Opiekun Koła - dr hab. Anna Cierpka',
-  text: "'Jestem psychologiem, adiunktem Wydziału Psychologii Uniwersytetu Warszawskiego. Moje zainteresowania naukowe skupiają się wokół rodziny jako środowiska wychowawczego oraz tematyki narracji. Jestem współredaktorem książek Narracja: koncepcje i badania psychologiczne oraz Psychologia narracyjna. Tożsamość, dialogowość, pogranicza i autorem wielu tekstów poświęconych problematyce narracji. W praktyce psychologicznej wykorzystuję założenia terapii systemowej i techniki terapii narracyjnej. Hobby to przede wszystkim dbanie o ogród i kwiaty. I podróże.'",
+  text: "Jestem psychologiem, adiunktem Wydziału Psychologii Uniwersytetu Warszawskiego. Moje zainteresowania naukowe skupiają się wokół rodziny jako środowiska wychowawczego oraz tematyki narracji. Jestem współredaktorem książek "Narracja: koncepcje i badania psychologiczne" oraz "Psychologia narracyjna. Tożsamość, dialogowość, pogranicza" i autorem wielu tekstów poświęconych problematyce narracji. W praktyce psychologicznej wykorzystuję założenia terapii systemowej i techniki terapii narracyjnej. Hobby to przede wszystkim dbanie o ogród i kwiaty. I podróże.",
 }
 
 export const administration = {
   title: 'Zarząd',
-  text: 'Rok akademicki 2023/24',
+  text: 'Rok akademicki 2024/25',
   info: [
     {
-      title: 'Marcin Bagiński',
-      text: 'Prezes Koła',
-      img: '/images/team/mb.png',
-    },
-    {
-      title: 'Karolina Marcinkowska',
-      text: 'Specjalistka ds. naukowych',
-      img: '/images/team/km.png',
-    },
-    {
       title: 'Julia Ukielska',
-      text: 'Wsparcie Zarządu',
+      text: 'Prezeska Koła',
       img: '/images/team/ju.png',
     },
     {
       title: 'Julia Miążek',
-      text: 'Wsparcie Zarządu',
+      text: 'Prezeska Koła',
       img: '/images/team/jm.png',
+    },
+    {
+      title: 'Wiktoria Vincent Kopecka',
+      text: 'Członek Zarządu',
+      img: '/images/team/wvk.png',
+    },
+    {
+      title: 'Natalia Koźmińska',
+      text: 'Członkini Zarządu',
+      img: '/images/team/nk.png',
     },
   ],
 }
